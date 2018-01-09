b6781bef8a1fc70a1be7963a4dab1d882b03bb35
NVLLL <13753164702@163.com>
2018-01-09 23:29:30 +0800

    Python基础--dict和set

---
+++ 基础语法/08_dict和set.py
@@ -0,0 +1,59 @@
+# dict 同java中的map，使用键-值(key-value)存储数据
+d = {'Michael':95,'Bob':88,'Tracy':60}
+print(d['Michael'])
+
+# 把数据放入dict，除了初始化时指定外，还可以通过key放入：
+d['Jerry'] = 70
+print(d['Jerry'])
+
+# 多次对同一个key放入value，后者会将前者覆盖
+d['Jerry'] = 90
+print(d['Jerry'])
+
+# 如何判断dict中是否存在某个key
+# 第一种方式：
+print('Bob' in d)
+# 第二种方式：通过dict提供的get(key)方法，如果key不存在返回None；如果存在则返回对应的value
+print(d.get('Bob'))
+print(d.get('Tom'))
+# 还可以指定当key不存在时返回特定的值，示例：
+print(d.get('Tom',-1)) # 当dict中不存在key='Tom'时，返回-1
+
+# pop(key):删除特定的key,返回值为key所对应的value
+d.pop('Jerry')
+print(d)
+
+# 注意：dict中数据的存取不一致
+
+# dict利用key通过hash算法来计算value的存储位置，所以不可变的对象(例如，整数、字符串等)才可以作为key，而list是可变的就不能作为key
+l = [1,'b',3.122]
+# d[l] = 'Hello'
+# print(d[l])
+t = ('a','b',1)         # 可以使用tuple作为dict的key
+d[t] = 'Hello'
+print(d[t])
+# t1 = ('a','b',[1,2])
+# d[t1] = 'python'      # 报错：tuple中有可变元素list
+# print(d[t1])
+
+
+# set 创建set需要传入一个list作为输入集合
+s = set([1,2,3,3,2])
+print(s)
+# 注意：
+#   1、set同java中的一样，不能存重复元素
+#   2、set中的元素存取不一致
+#   3、同dict一样，set中也不可以存可变的对象，因为无法判断两个可变对象是否相等，也就无法保证set内没有重复元素
+
+# 向set中添加元素：add()
+s.add('Tom')
+print(s)
+
+# 删除set中的元素：remove()
+s.remove(1)
+print(s)
+# set可以看成数学意义上的无序和无重复元素的集合,所以两个set可以做交集、并集等操作
+s1 = set(['a','b','c'])
+s2 = set(['c','e','f'])
+print(s1 & s2)  # {'c'}
+print(s1 | s2)  # {'c', 'f', 'a', 'b', 'e'}
